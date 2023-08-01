244eac2013098512b90b0d5f9fba22ec331794a0
Sachin Gurung <saching061@hotmail.com>
2023-08-01 12:47:41 -0700

    For Loop. Print Even Numbers in console

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -3,28 +3,12 @@
 namespace HelloWorld {
     public class Program {
         static void Main(string[] args) {
-            Console.Write("Enter the speed limit: ");
-            int speedLimit = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Enter your current speed: ");
-            int currentSpeed = Convert.ToInt32(Console.ReadLine());
-
-            int demeritPoint = 0;
-
-            if (currentSpeed <= speedLimit) {
-                Console.WriteLine("OK");
-            } else {
-                int speedDifference = currentSpeed - speedLimit;
-                for (int counter = 1; counter <= speedDifference; counter++) {
-                    if (counter % 5 == 0) {
-                        demeritPoint++;
-                    }
+            int evenNumber;
+            for (int i = 0; i <= 10; i++) {
+                if (i % 2 == 0) {
+                    Console.WriteLine("{0} is even number", i);
                 }
-            }
-
-            Console.WriteLine("Demerit points: {0}", demeritPoint);
-            if (demeritPoint >= 12) {
-                Console.WriteLine("License suspended!!!");
+                i++;
             }
         }
     }
