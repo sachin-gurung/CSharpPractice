153a972534f70de664be89ed398144c827d07cfa
Sachin Gurung <saching061@hotmail.com>
2023-08-02 15:36:29 -0700

    Arrays

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -3,6 +3,7 @@
 namespace HelloWorld {
     public class Program {
         static void Main(string[] args) {
+            //Rectangular Array
             var rectangularArray = new int[3, 5]
             {
                 {1,2,3,4,5},
@@ -12,18 +13,31 @@ namespace HelloWorld {
 
             Console.WriteLine(rectangularArray[2, 4]);
 
+            //Jagged Array
             var jaggedArray = new int[3][];
 
             var array1 = new int[3] { 0, 1, 2 };
             var array2 = new int[4] { 3, 4, 5, 6 };
             var array3 = new int[2] { 7, 8 };
 
-            int v = array1[0];
-            Console.WriteLine(v);
+            Console.WriteLine(array1[0]);
             Console.WriteLine(array2[1]);
 
-            //Console.WriteLine(jaggedArray[array1[0]][array2[1]]);
-            Console.WriteLine(jaggedArray[v] array2[1]);
+            Console.WriteLine(array1.Length);
+            Console.WriteLine(Array.IndexOf(array1, 2));
+
+            //Clear - Sets the numbers to 0 when cleared, Boolean to false, Strings to null.
+            Array.Clear(array2, 1, 2);
+            foreach (var i in array2) {
+                Console.WriteLine("array 4: " + i);
+            }
+
+            //Copy
+            var array4 = new int[3];
+            Array.Copy(array1, array4, 2);
+            foreach (var i in array4) {
+                Console.WriteLine(i);
+            }
         }
     }
 }
