dc539718a3a0d1493da59c011c4816c03a590b21
Sachin Gurung <saching061@hotmail.com>
2023-08-02 13:14:48 -0700

    Write a program and ask the user to enter a series of numbers separated by comma. Find the maximum of the numbers and display it on the console. For example, if the user enters 5, 3, 8, 1, 4, the program should display 8.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,25 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HelloWorld {
     public class Program {
+
+        /*
+         * Write a program and ask the user to enter a series of numbers separated by comma. Find the maximum of the numbers and display it on the console. For example, if the user enters “5, 3, 8, 1, 4", the program should display 8.
+         */
         static void Main(string[] args) {
-            int randomNumber = new Random().Next(1, 10);
-            int userGuessedNumber;
-            int numberOfChances = 4;
-            Console.WriteLine(randomNumber);
+            Console.Write("Enter a series of numbers separated by comma: ");
+            var input = Console.ReadLine();
+            var numbers = GetListFromValuesSeparatedWithCommas(input);
+            var maxNumber = numbers?.Max();
+            Console.WriteLine(maxNumber);
+        }
 
-            for (int i = 0; i < numberOfChances; i++) {
-                Console.Write("Guess the correct number: ");
-                userGuessedNumber = Convert.ToInt32(Console.ReadLine());
-                if (randomNumber == userGuessedNumber) {
-                    Console.WriteLine("You win");
-                    break;
-                } else {
-                    Console.WriteLine("Incorrect");
-                }
-            }
-
-            Console.WriteLine("Game over....");
+        private static IEnumerable<int> GetListFromValuesSeparatedWithCommas(string input) {
+            return input?.Split(',').Select(number => Convert.ToInt32(number.Trim())).ToList();
         }
     }
 }
