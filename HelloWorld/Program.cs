5848d2df17d9e7e39f9a184da9df41aba152e3b9
Sachin Gurung <saching061@hotmail.com>
2023-08-01 17:10:50 -0700

    Write a program and continuously ask the user to enter a number or ok to exit. Calculate the sum of all the previously entered numbers and display it on the console.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -3,17 +3,22 @@
 namespace HelloWorld {
     public class Program {
         static void Main(string[] args) {
-            int count = 0;
-            int minValue = 1;
-            int maxValue = 100;
 
-            for (int i = 0; i < maxValue; i++) {
-                if (i % 3 == 0) {
-                    count++;
+            int sum = 0;
+
+            while (true) {
+
+                Console.Write("Enter a number or \"ok\" to exit:");
+                var input = Console.ReadLine();
+
+                if (input == "ok") {
+                    break;
                 }
-            }
 
-            Console.WriteLine(count);
+                var number = Convert.ToInt32(input);
+                sum += number;
+            }
+            Console.WriteLine(sum);
         }
     }
 }
