5676fc99c1085583fe0f8571548586921ffd1570
Gurung <sachin.gurung2.ctr@us.navy.mil>
2023-08-03 09:05:03 -0700

    [200~1- When you post a message on Facebook, depending on the number of people who like your post, Facebook displays different information.
    If no one likes your post, it doesn't display anything.
    If only one person likes your post, it displays: [Friend's Name] likes your post.
    If two people like your post, it displays: [Friend 1] and [Friend 2] like your post.
    If more than two people like your post, it displays: [Friend 1], [Friend 2] and [Number of Other People] others like your post.
    Write a program and continuously ask the user to enter different names, until the user presses Enter (without supplying a name). Depending on the number of names provided, display a message based on the above pattern.~

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,62 +4,30 @@ using System.Collections.Generic;
 namespace HelloWorld {
     public class Program {
         static void Main(string[] args) {
-<<<<<<< HEAD
-            //Rectangular Array
-            var rectangularArray = new int[3, 5]
-            {
-                {1,2,3,4,5},
-                {6,7,8,9,10},
-                {11,12,13,14,15}
-            };
-=======
-            var numbers = new List<int>() { 1, 2, 3 };
-            numbers.Add(4);    //Add
-            numbers.AddRange(new int[3] { 5, 6, 7 });    //AddRange
-            foreach (var i in numbers) {
-                Console.WriteLine(i);
-            }
->>>>>>> SachinSandBox
-
-            Console.WriteLine("Index of 2: " + numbers.IndexOf(3));  //IndexOf
-            Console.WriteLine("Last Index of 2: " + numbers.LastIndexOf(3));  //LastIndexOf
-
-<<<<<<< HEAD
-            //Jagged Array
-            var jaggedArray = new int[3][];
-
-            var array1 = new int[3] { 0, 1, 2 };
-            var array2 = new int[4] { 3, 4, 5, 6 };
-            var array3 = new int[2] { 7, 8 };
-
-            Console.WriteLine(array1[0]);
-            Console.WriteLine(array2[1]);
-
-            Console.WriteLine(array1.Length);
-            Console.WriteLine(Array.IndexOf(array1, 2));
-
-            //Clear - Sets the numbers to 0 when cleared, Boolean to false, Strings to null.
-            Array.Clear(array2, 1, 2);
-            foreach (var i in array2) {
-                Console.WriteLine("array 4: " + i);
+            var nameList = new List<string>();
+            while (true) {
+                Console.Write("Enter user name: ");
+                var name = Console.ReadLine();
+                nameList.Add(name);
+                
+                if (String.IsNullOrWhiteSpace(name)) {
+                    break;
+                }
             }
 
-            //Copy
-            var array4 = new int[3];
-            Array.Copy(array1, array4, 2);
-            foreach (var i in array4) {
-                Console.WriteLine(i);
-            }
-=======
-            Console.WriteLine("Count: " + numbers.Count); //Count
-            numbers.Remove(1);    //Remove
-            foreach (var i in numbers) {
-                Console.WriteLine("After removing: " + i);
+            if (nameList.Count == 0) {
+                Console.WriteLine("Nobody likes your post");
+            } else if (nameList.Count == 1) {
+                Console.WriteLine("{0} likes your post", nameList[0]);
+            } else if (nameList.Count == 2) {
+                Console.WriteLine("{0} and {1} like your post", nameList[0], nameList[1]);
+            } else {
+                Console.WriteLine("{0}, {1} and others like your post", nameList[0], nameList[1]);
             }
 
-            numbers.Clear();
-            Console.WriteLine("Total count after clear(): " + numbers.Count);
->>>>>>> SachinSandBox
+            //The code below is just to test if it's saving all the names in the list
+            Console.WriteLine("List of names are below:");
+            nameList.ForEach(name => Console.WriteLine(name));
         }
     }
 }
