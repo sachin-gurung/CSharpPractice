c2b62ff756c51fcdd1322b4d3522f415a0a51795
Sachin Gurung <saching061@hotmail.com>
2023-08-07 07:32:06 -0700

    Write a program and ask the user to continuously enter a number or type Quit to exit. The list of numbers may include duplicates.Display the unique numbers that the user has entered.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,65 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace HelloWorld {
     public class Program {
         static void Main(string[] args) {
-<<<<<<< HEAD
-            //Rectangular Array
-            var rectangularArray = new int[3, 5]
+            /*Write a program and ask the user to continuously enter a number or type "Quit" to exit. The list of numbers may include duplicates.Display the unique numbers that the user has entered.
+             */
+
+            var uniqueList = new List<int>();
+
+            while (true)
             {
-                {1,2,3,4,5},
-                {6,7,8,9,10},
-                {11,12,13,14,15}
-            };
-=======
-            var numbers = new List<int>() { 1, 2, 3 };
-            numbers.Add(4);    //Add
-            numbers.AddRange(new int[3] { 5, 6, 7 });    //AddRange
-            foreach (var i in numbers) {
-                Console.WriteLine(i);
-            }
->>>>>>> SachinSandBox
+                Console.Write("Enter a number or type \"Quit\" to exit: ");
+                var input = Console.ReadLine();
 
-            Console.WriteLine("Index of 2: " + numbers.IndexOf(3));  //IndexOf
-            Console.WriteLine("Last Index of 2: " + numbers.LastIndexOf(3));  //LastIndexOf
-
-<<<<<<< HEAD
-            //Jagged Array
-            var jaggedArray = new int[3][];
-
-            var array1 = new int[3] { 0, 1, 2 };
-            var array2 = new int[4] { 3, 4, 5, 6 };
-            var array3 = new int[2] { 7, 8 };
-
-            Console.WriteLine(array1[0]);
-            Console.WriteLine(array2[1]);
-
-            Console.WriteLine(array1.Length);
-            Console.WriteLine(Array.IndexOf(array1, 2));
-
-            //Clear - Sets the numbers to 0 when cleared, Boolean to false, Strings to null.
-            Array.Clear(array2, 1, 2);
-            foreach (var i in array2) {
-                Console.WriteLine("array 4: " + i);
+                if (input.CompareTo("quit") == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    var number = Convert.ToInt32(input);
+                    if (uniqueList.Contains(number))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        uniqueList.Add(number);
+                    }
+                }
             }
 
-            //Copy
-            var array4 = new int[3];
-            Array.Copy(array1, array4, 2);
-            foreach (var i in array4) {
-                Console.WriteLine(i);
+            foreach (var uniqueNum in uniqueList) {
+                Console.WriteLine("{0}", uniqueNum);
             }
-=======
-            Console.WriteLine("Count: " + numbers.Count); //Count
-            numbers.Remove(1);    //Remove
-            foreach (var i in numbers) {
-                Console.WriteLine("After removing: " + i);
-            }
-
-            numbers.Clear();
-            Console.WriteLine("Total count after clear(): " + numbers.Count);
->>>>>>> SachinSandBox
         }
     }
 }
