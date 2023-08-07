60a8772381c541fbec1c67dd52360fd26bc0ca2f
Sachin Gurung <saching061@hotmail.com>
2023-08-07 14:29:56 -0700

    StringBuilder()

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,38 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
+using System.Text;
 
 namespace HelloWorld {
     public class Program {
         static void Main(string[] args)
         {
-            var sentence = "This is a very very long sentence. It can be longer than this but I think this is enough";
-            var maxLength = 20;
+            var builder = new StringBuilder();
+            builder.Append("New line").AppendLine().Append("Another new line").Replace("N", "n");
 
-            if (sentence.Length <maxLength)
-            {
-                Console.WriteLine(sentence);
-                Console.ReadLine();
-            }
-            else
-            {
-                var words = sentence.Split(' ');
-                var totalCharacterCount = 0;
-                var summaryWords = new List<string>();
-
-                foreach (var word in words)
-                {
-                    summaryWords.Add(word);
-
-                    totalCharacterCount += word.Length + 1;
-                    if (totalCharacterCount > maxLength)
-                        break;
-                }
-
-                var summary = String.Join(" ",summaryWords) + "...";
-                Console.WriteLine(summary);
-                Console.ReadLine();
-            }
+            Console.WriteLine(builder);
+            Console.ReadLine();
         }
     }
 }
