dc6203947dccfd60070162e5d7211045a3cc19c3
Sachin Gurung <saching061@hotmail.com>
2023-08-07 14:49:30 -0700

    Write a program and ask the user to enter a few numbers separated by a hyphen. Work out if the numbers are consecutive. For example, if the input is "5-6-7-8-9" or "20-19-18-17-16", display a message: "Consecutive"; otherwise, display "Not Consecutive".

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,11 +8,30 @@ namespace HelloWorld {
     public class Program {
         static void Main(string[] args)
         {
-            var builder = new StringBuilder();
-            builder.Append("New line").AppendLine().Append("Another new line").Replace("N", "n");
+            /*
+             * Write a program and ask the user to enter a few numbers separated by a hyphen. Work out if the numbers are consecutive. For example, if the input is "5-6-7-8-9" or "20-19-18-17-16", display a message: "Consecutive"; otherwise, display "Not Consecutive".
+             */
+            Console.Write("Enter a few numbers (eg 1-2-3-4): ");
+            var input = Console.ReadLine();
 
-            Console.WriteLine(builder);
+            var numbers = new List<int>();
+            foreach (var number in input.Split('-'))
+                numbers.Add(Convert.ToInt32(number));
+
+            numbers.Sort();
+
+            var isConsecutive = true;
+            for (var i = 1; i < numbers.Count; i++) {
+                if (numbers[i] != numbers[i - 1] + 1) {
+                    isConsecutive = false;
+                    break;
+                }
+            }
+
+            var message = isConsecutive ? "Consecutive" : "Not Consecutive";
+            Console.WriteLine(message);
             Console.ReadLine();
+
         }
     }
 }
