5f7b8f2846d3044372be09a9b294215309254db3
Sachin Gurung <saching061@hotmail.com>
2023-08-07 13:28:07 -0700

    Some practice with Strings

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,34 +8,18 @@ namespace HelloWorld {
         /*
          * Write a program and ask the user to enter 5 numbers. If a number has been previously entered, display an error message and ask the user to re-try. Once the user successfully enters 5 unique numbers, sort them and display the result on the console.
          */
-        static void Main(string[] args) {
-			var list = new List<int>();
-            while (true)
+        static void Main(string[] args)
+        {
+            var name = "Sachin Gurung ";
+            var trimmedName = name.Trim();
+            Console.WriteLine("'"+trimmedName+"'");
+            Console.WriteLine(trimmedName.ToUpper());
+            var names = name.Split(' ');
+            foreach (var i in names)
             {
-                Console.WriteLine("Type a list of number, separated by commas");
-                var input = Console.ReadLine();
-
-                var array = input.Split(',');
-
-                if ((array.Length == 0) || (array.Length < 5))
-                {
-                    Console.WriteLine("Invalid list! Try again.");
-                }
-                else
-                {
-                    foreach (var number in array)
-                        list.Add(Convert.ToInt32(number));
-                    break;
-                }
-
-                list.Sort();
-
-                for (int i = 0; i < 3; i++)
-                {
-                    Console.Write("{0} ", list[i]);
-                }
+                Console.WriteLine(i);
             }
-
+            Console.ReadLine();
         }
     }
 }
