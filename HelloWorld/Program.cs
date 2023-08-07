10ef63b38abe14e778edc96b47490b27de422f80
Sachin Gurung <saching061@hotmail.com>
2023-08-07 10:40:58 -0700

    ask the user to supply a list of comma separated numbers. If the list is empty or includes less than 5 numbers, display Invalid List and ask the user to retry. otehrwise display the 3 smalles numbers int he list

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HelloWorld {
     public class Program {
         static void Main(string[] args) {
-            int evenNumber;
-            for (int i = 0; i <= 10; i++) {
-                if (i % 2 == 0) {
-                    Console.WriteLine("{0} is even number", i);
+			/*
+             * Write a program and ask the user to supply a list of comma separated numbers (e.g 5, 1, 9, 2, 10). If the list is empty or includes less than 5 numbers, display "Invalid List" and ask the user to re-try; otherwise, display the 3 smallest numbers in the list.
+             */
+			var list = new List<int>();
+            while (true) {
+                Console.WriteLine("Type a list of number, separated by commas");
+                var input = Console.ReadLine();
+
+                var array = input.Split(',');
+
+                if ((array.Length == 0) || (array.Length < 5)) {
+                    Console.WriteLine("Invalid list! Try again.");
+                } else {
+                    foreach (var number in array)
+                        list.Add(Convert.ToInt32(number));
+                    break;
                 }
-                i++;
             }
+            list.Sort();
+
+            for (int i = 0; i < 3; i++)
+                Console.Write("{0} ", list[i]);
         }
     }
 }
