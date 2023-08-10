44143129692fe0d2952954be805d2e3e9307e8e9
Sachin Gurung <saching061@hotmail.com>
2023-08-09 18:51:38 -0700

    Constructors

---
+++ HelloWorld/Animal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld {
+    class Animal {
+        //member arguments
+        string _name;
+        bool _isCarnivorous;
+        int _age;
+
+        public Animal()
+        {
+            Console.WriteLine("I don't have any arguments!!");
+        }
+
+        public Animal(string name)
+        {
+            this._name = name;
+            Console.WriteLine("I have a name and my name is {0}", name);
+        }
+        public Animal(string name, bool isCarnivorous, int age)
+            :this()
+        {
+            this._isCarnivorous = isCarnivorous;
+            this._age = age;
+
+            Console.WriteLine("{0} is {1} year old!!",name,age);
+        }
+    }
+}
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,43 +8,11 @@ namespace HelloWorld {
     public class Program {
         static void Main(string[] args)
         {
-            /*
-             * Write a program and ask the user to enter a few numbers separated by a hyphen. If the user simply presses Enter, without supplying an input, exit immediately; otherwise, check to see if there are duplicates. If so, display "Duplicate" on the console.
-             */
-            Console.WriteLine("Enter a few numbers separated by hyphen (eg 1-3-4-5): ");
-            var input = Console.ReadLine();
-
-            if (string.IsNullOrWhiteSpace(input))
-                return;
-
-            var numbers = new List<int>();
-            foreach (var number in input.Split('-'))
-            {
-                numbers.Add(Convert.ToInt32(number));
-            }
-
-            numbers.Sort();
-            var isDuplicate = false;
-            var uniques = new List<int>();
-
-            foreach (var number in numbers)
-            {
-                if (!uniques.Contains(number))
-                {
-                    uniques.Add(number);
-                }
-                else
-                {
-                    isDuplicate = true;
-                    break;
-                }
-            }
-
-            if (isDuplicate)
-            {
-                Console.WriteLine("Duplicates!");
-                Console.ReadLine();
-            }
+            //Constructors
+            var cat1 = new Animal();
+            var cat2 = new Animal("Mike");
+            var cat3 = new Animal("Salli", true,20);
+            Console.ReadLine();
         }
     }
 }
