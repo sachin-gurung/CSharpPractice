4e78a53a09dd4cd22e94e8b08c5ce657e1907ccd
Sachin Gurung <saching061@hotmail.com>
2023-08-14 22:10:33 -0700

    Inheritence

---
+++ HelloWorld/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld {
+    public class Animal {
+        private int Age { get; set; }
+        private int Name { get; set; }
+
+        public string makeSound() {
+            return "Animals can make sound";
+            }
+
+        public void eat() {
+            Console.WriteLine("Animals can eat");
+            }
+
+        }
+    }
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -3,23 +3,12 @@
 namespace HelloWorld {
     public class Program {
 
-        public class Person {
-
-            //Here, the "set" is private, so it can only be set in the constructor. 
-
-            //This is a constructor. It can set the values of the private set properties.
-            public Person(string name, int age) {
-                Name = name;
-                Age = age;
-                }
-
-            public string Name { get; private set; } //"set" is private
-            public int Age { get; private set; }
-            }
-
         static void Main(string[] args) {
-            var person = new Person("John", 20);
-            Console.WriteLine(person.Age);
+            var cat1 = new cat();
+            var animal1 = new Animal();
+
+            Console.WriteLine(cat1.makeSound());
+            Console.WriteLine(animal1.makeSound());
             Console.ReadLine();
 
             }
