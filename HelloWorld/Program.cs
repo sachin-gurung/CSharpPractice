65290aed79a6791178334a6ab3072f33e0a6a55b
Sachin Gurung <saching061@hotmail.com>
2023-08-16 13:26:18 -0700

    Virtual and Override methods

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,9 +1,31 @@
-﻿namespace HelloWorld {
+﻿using System;
+
+namespace HelloWorld {
+    public class Shape {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public virtual void Draw() {
+            Console.WriteLine("Draw a shape");
+            }
+        }
+
+    public class Circle : Shape {
+        public override void Draw() {
+            Console.WriteLine("Draw a circle");
+            }
+        }
+
     public partial class Program {
         static void Main(string[] args) {
-
+            var circle = new Circle();
+            circle.Draw();
+            Console.ReadLine();
             }
         }
     }
+
 
 
