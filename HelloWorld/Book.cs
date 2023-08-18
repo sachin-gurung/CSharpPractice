cfa4a08532417c14097e5302e4aa90e5788e728b
Sachin Gurung <saching061@hotmail.com>
2023-08-18 14:50:23 -0700

    Lambda Expressions

--- HelloWorld/Book.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace HelloWorld {
-    public class Book {
-        public string Isbn { get; internal set; }
-        public string Title { get; internal set; }
-        }
-    }
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,24 +1,17 @@
-﻿namespace HelloWorld {
+﻿using System;
+
+namespace HelloWorld {
     /*
      * We rarely have to make Generic list. But this is the way to make it. Instead of making a list for each type, we can make a generic list. Instead of making bookList, IntList, ObjectList, we can make a generic list. This way, we can make a list for any type and we don't lose performance because we are not using object type.
      */
 
     public partial class Program {
         static void Main(string[] args) {
-            var book1 = new Book { Isbn = "1111", Title = "C# Advanced" };
 
-            var numbers = new GenericList<int>();
-            numbers.Add(10);
-            numbers.Add(20);
-
-            var books = new GenericList<Book>();
-            books.Add(new Book());
-
-            //System.Collections.Generic.
-
-            var dictionary = new GenericDictionary<string, Book>();
-            dictionary.Add("1234", new Book());
-            dictionary.Add("1235", new Book());
+            // args => expression
+            Func<int, int> square = number => number * number;    // number => number * number is a lambda expression. It is a function that takes a number and returns the square of that number.
+            // Func<int, int> is a delegate. It is a pointer to a function. It is a type that can point to a function that takes an int and returns an int.
+            Console.WriteLine(square(5));
             }
         }
     }
